5e65dd8f79a3f66a7c5a1ccc4623635561f016b4
faria-r <fariacse11@gmail.com>
2022-11-03 09:54:51 +0600

    secured env file

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.json())
 //mongodb connecting
 
 
-const uri = "mongodb+srv://DB_USER:DB_PASS@cluster0.wxeycza.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wxeycza.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
